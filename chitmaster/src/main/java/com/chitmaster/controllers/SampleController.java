24a6be8280888eab0f371a38b99e1270b62b0d63
vishal <durgadvis@gmail.com>
2017-03-11 23:12:11 +0530

    chitmaster project | the begining

---
+++ chitmaster/src/main/java/com/chitmaster/controllers/SampleController.java
@@ -0,0 +1,15 @@
+package com.chitmaster.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/chitmaster/getname")
+	public String getName(){
+		
+		return "Vishal-Shruthi";
+	}
+	
+}
